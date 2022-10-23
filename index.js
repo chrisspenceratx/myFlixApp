9f6089ada4c088a9570f8495624ab1f242eba854
chrisspenceratx <chrisspenceratx@gmail.com>
2022-10-23 05:56:50 -0500

    still working out connection issue

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ const allowedOrigins = ["*"];
 
 //below code shows origins that are authorized//
 // let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 /* rest rest of code goes here*/
 let auth = require('./auth')(app);
