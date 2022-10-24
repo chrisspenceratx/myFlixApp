23a1035e93ecf5527fb43d3a1acef3e299aaafb1
chrisspenceratx <chrisspenceratx@gmail.com>
2022-10-24 16:51:27 -0500

    setting to local temporary

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const Movies = Models.Movie;
 const Users = Models.User;
 
 // local database
-//mongoose.connect('mongodb://localhost:27017/movieDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/movieDB', {useNewUrlParser: true, useUnifiedTopology: true});
 // online database
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // MIDDLEWARE
 app.use(bodyParser.json());
