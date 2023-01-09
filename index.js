f2f549d270d8ae80ba5e47270a0cbcff1c6b226a
chrisspenceratx <chrisspenceratx@gmail.com>
2023-01-09 14:50:07 -0600

    turned port back on for heroku

--- index.js
+++ index.js
@@ -467,9 +467,9 @@ app.listen(8080, () => {
 
 
 // Widened accesibility from port 8080 only //
-// const port = process.env.PORT || 8080;
-// app.listen(port, '0.0.0.0',() => {
-//  console.log('Listening on Port ' + port);
-// });//
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});//
 
 
